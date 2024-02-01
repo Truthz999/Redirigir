a01f998bddba67f0db2a8f74a63103117ad839be
Truth <138615922+Jorgeepython@users.noreply.github.com>
2024-02-01 14:11:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Conecta a web "amiga"
+
+
+import pyautogui
+import time
+
+pyautogui.press("win")
+time.sleep(0.5)
+
+pyautogui.write("https://www.youtube.com/watch?v=YtmqU4J-5X8")
+time.sleep(0.5)
+pyautogui.press("enter")
+
